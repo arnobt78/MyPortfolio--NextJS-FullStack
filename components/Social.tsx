20cb098cd70d7c2dff10b464744b447a9ce433e8
Arnob Mahmud <arnob_t78@yahoo.com>
2026-01-23 15:43:56 +0100

    add: x.com link to social media icons

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaYoutube, FaInstagram, FaFacebook } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaYoutube,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const socials = [
@@ -11,7 +17,7 @@ const socials = [
   { icon: <FaYoutube />, path: "https://www.youtube.com/@arnobcorleone8570" },
   { icon: <FaInstagram />, path: "https://www.instagram.com/arnob_t78/" },
   { icon: <FaFacebook />, path: "https://www.facebook.com/arnob.mahmud/" },
-  { icon: <FaXTwitter />, path: "https://x.com/home" },
+  { icon: <FaXTwitter />, path: "https://x.com/arnob_t78" },
 ];
 
 interface SocialProps {
