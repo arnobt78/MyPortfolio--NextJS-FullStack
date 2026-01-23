e4d7299b6f2b1c3732d1b57365295f0bab35bdfa
Arnob Mahmud <arnob_t78@yahoo.com>
2026-01-23 15:42:40 +0100

    feat: Add Facebook and Twitter icons to homepage social links
    - Add FaFacebook icon from react-icons/fa
    - Add FaXTwitter icon from react-icons/fa6
    - Add Facebook link: https://www.facebook.com/arnob.mahmud/
    - Add Twitter/X link: https://x.com/home
    - Maintain same responsive styling and layout as existing social icons
    - Icons display in same order: GitHub, LinkedIn, YouTube, Instagram, Facebook, Twitter

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaYoutube, FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaInstagram, FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const socials = [
   { icon: <FaGithub />, path: "https://github.com/arnobt78" },
@@ -9,6 +10,8 @@ const socials = [
   },
   { icon: <FaYoutube />, path: "https://www.youtube.com/@arnobcorleone8570" },
   { icon: <FaInstagram />, path: "https://www.instagram.com/arnob_t78/" },
+  { icon: <FaFacebook />, path: "https://www.facebook.com/arnob.mahmud/" },
+  { icon: <FaXTwitter />, path: "https://x.com/home" },
 ];
 
 interface SocialProps {
